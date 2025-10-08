7142d959f9f1e9019606f14de9c5798c87934674
Flavio Serra <sonyflavio@gmail.com>
2025-10-08 11:09:35 -0300

    Criando a Entity Produto e Module

---
+++ src/produto/entities/produto.entity.ts
@@ -0,0 +1,38 @@
+import { IsNotEmpty, IsNumber } from "class-validator";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({name: "tb_produtos"})
+export class Produto {
+  
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Column('decimal', { precision: 10, scale: 2, nullable: false })
+  preco: number;
+
+
+  @Column({ length: 300, nullable: false })
+  descricao: string;
+
+  @Column({ type: 'text', nullable: false })
+  foto: string;
+
+  
+
+
+
+ 
+  
+
+  
+}
+
+}
