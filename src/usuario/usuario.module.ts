ae5355488e189ebec68e4022f37103fa885322d1
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-08 12:45:49 -0300

    feat(usuario): implementação da classe UsuarioModule

---
+++ src/usuario/usuario.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Usuario])],
+  providers: [UsuarioService],
+  controllers: [UsuarioController],
+  exports: [UsuarioService],
+})
+export class UsuarioModule {}
