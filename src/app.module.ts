31db3c9b9b0b32c86eb7d0be0357fcaa1c59c876
Sthefanyom <om.sthefany@gmail.com>
2025-10-08 22:31:58 -0300

    Corrige configuração de conexão com o banco no Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Produto } from './produto/entities/produto.entity';
-import { Categoria } from './categoria/entities/categoria.entity';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { ProdutoModule } from './produto/produto.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 // conexao com o banco de dados atraves de TypeORM
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_arara_delivery',
-      entities: [Produto, Categoria, Usuario],
-      synchronize: true,
-      // logging: true, //opcional
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     ProdutoModule,
     CategoriaModule,
